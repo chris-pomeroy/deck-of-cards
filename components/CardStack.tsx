cd8bbaec8322bfc730b4b8e23ba11a946b34a2b7
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-04-23 20:40:44 -0600

    Added rewind and back buttons

--- components/CardStack.tsx
+++ components/CardStack.tsx
@@ -1,6 +1,7 @@
 import * as CardImages from '../assets/cards';
-import CardBack from '../assets/cards/back.svg';
-import {SafeAreaView} from "react-native";
+import BackIcon from '../assets/icons/back.svg';
+import RewindIcon from '../assets/icons/rewind.svg';
+import {SafeAreaView, TouchableOpacity, View} from "react-native";
 import {FC, useState} from "react";
 import {Card} from "@/components/Card";
 import {SvgProps} from "react-native-svg";
@@ -38,6 +39,39 @@ export default function CardStack() {
                     key={cardName}
                 />
             ))}
+            <View style={{
+                position: "absolute",
+                bottom: 50,
+                flexDirection: "row",
+                display: currentCard > 0 ? "flex" : "none"
+            }}>
+                <TouchableOpacity
+                    style={{
+                        zIndex: CARDS_TO_RENDER + 1,
+                    }}
+                    onPress={() => setCurrentCard(0)}>
+                    <RewindIcon
+                        width={100}
+                        height={100}
+                        style={{
+                            marginRight: 20
+                        }}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{
+                        zIndex: CARDS_TO_RENDER + 1,
+                    }}
+                    onPress={() => setCurrentCard(prev => prev - 1)}>
+                    <BackIcon
+                        width={100}
+                        height={100}
+                        style={{
+                            marginRight: 20
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
